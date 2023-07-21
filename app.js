e3c0f9389db3c2b6a6fcda6882901d74d6807b63
Priyanka <yashikalaller@gmail.com>
2023-07-21 14:57:49 +1000

    added glossary

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ const glossary = [
   {
     id: 1,
     term: 'Node.js',
-    definition: 'A JavaScript runtime built on Chrome\'s V8 JavaScript engine.',
-    references: 'https://nodejs.org/'
+    definition: 'Node.js is an open-source, server-side JavaScript runtime environment built on Chrome V8 JavaScript engine. It allows developers to execute JavaScript code outside of a web browser, enabling server-side scripting to create dynamic web applications.',
+    references: 'Node.js Foundation. (2022, January 25). "Node.js." Node.js Foundation. Available online: https://nodejs.org/ (accessed July 21, 2023).'
   },
   {
     id: 2,
     term: 'Express',
-    definition: 'A web application framework for Node.js.',
-    references: 'https://expressjs.com/'
+    definition: 'Express.js is a Node.js web application framework that is quick and easy free from bias, and simple. It offers a variety of powerful features and tools for developing online applications and APIs, making it easier to manage HTTP requests, route traffic, use middleware, and serve static files.',
+    references: '[Expressjs. (2022, August 10). "Express.js." Expressjs. Available online: https://expressjs.com/ (accessed July 21, 2023)'
   },
+
   {
     id: 3,
     term: 'Git',
@@ -28,7 +29,7 @@ const glossary = [
     id: 4,
     term: 'DevOps',
     definition: 'DevOps is a set of techniques and tools created to help organizations create apps and services more quickly than with conventional software development methods.DevOps, to put it simply, aims to break down traditional divisions of work between development and operations teams. A DevOps framework involves development and operations teams working across the whole Software development lifecycle and testing to deployment and operations.',
-    references: '[1] Techopedia. (2022, July 15). "DevOps." Techopedia. Available online: https://www.techopedia.com/definition/25995/devops (accessed July 21, 2023).'
+    references: 'Techopedia. (2022, July 15). "DevOps." Techopedia. Available online: https://www.techopedia.com/definition/25995/devops (accessed July 21, 2023).'
   }
   // Add more terms and descriptions here
 ];
