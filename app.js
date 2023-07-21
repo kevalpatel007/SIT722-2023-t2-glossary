e1d3fcf2352299d376068d6de735ec8d65921d1a
Priyanka <yashikalaller@gmail.com>
2023-07-21 14:49:57 +1000

    Reference edit

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ const glossary = [
     definition: 'A web application framework for Node.js.',
     references: 'https://expressjs.com/'
   },
+  {
+    id: 3,
+    term: 'Git',
+    definition: 'A distributed version control system used to track changes in source code and manage collaborative development projects.',
+    references: 'https://git-scm.com/'
+  },
+  {
+    id: 4,
+    term: 'DevOps',
+    definition: 'DevOps is a set of techniques and tools created to help organizations create apps and services more quickly than with conventional software development methods.DevOps, to put it simply, aims to break down traditional divisions of work between development and operations teams. A DevOps framework involves development and operations teams working across the whole Software development lifecycle and testing to deployment and operations.',
+    references: '[1] Techopedia. (2022, July 15). "DevOps." Techopedia. Available online: https://www.techopedia.com/definition/25995/devops (accessed July 21, 2023).'
+  }
   // Add more terms and descriptions here
 ];
 
